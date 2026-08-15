f5412d0088882a1d7382a7c97ba35409bd97c6e1
najariwarisa-source <najariwarisa@gmail.com>
2026-08-15 15:06:21 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Isi langsung kodenya seperti ini
+const GEMINI_API_KEY = ""; 
+
+function renderOutline(items) {
+  // logika script Anda...
+}
+
+console.log("JavaScript berhasil dimuat dari file terpisah!");
